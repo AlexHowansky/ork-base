f6f08dde71992b83971f0300d0032297254127db
Alex Howansky <alex.howansky@gmail.com>
2015-09-20 18:47:25 +0000

    Added StringToTime filter.

---
+++ src/Filter/StringToTime.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Ork
+ *
+ * @package   Ork_Base
+ * @copyright 2015 Alex Howansky (https://github.com/AlexHowansky)
+ * @license   https://github.com/AlexHowansky/ork-base/blob/master/LICENSE MIT License
+ * @link      https://github.com/AlexHowansky/ork-base
+ */
+
+namespace Ork\Filter;
+
+/**
+ * Converts a string to a timestamp.
+ */
+class StringToTime implements FilterInterface
+{
+
+    use \Ork\ConfigurableTrait;
+
+    /**
+     * Configurable trait settings.
+     *
+     * @var array
+     */
+    protected $config = [
+
+        // Allow SQL strings like 'now()' and 'CURRENT_TIMESTAMP'
+        'allowSqlSpecials' => false,
+
+    ];
+
+    /**
+     * Convert a string to a timestamp.
+     *
+     * @param string $value A string to convert to a timestamp.
+     *
+     * @return integer The converted timestamp.
+     */
+    public function filter($value)
+    {
+
+        // If it's already a timestamp, return it as is.
+        if (preg_match('/^-?\d+$/', $value) === 1) {
+            if ($value < 0) {
+                throw new \DomainException('Invalid value');
+            }
+            return $value;
+        }
+
+        // Allow SQL specials.
+        $temp = strtolower($value);
+        if ($temp === 'now()' || $temp === 'current_timestamp') {
+            return time();
+        }
+
+        // Catch invalid strings.
+        $time = strtotime($value);
+        if ($time === false) {
+            throw new \DomainException('Invalid value');
+        }
+
+        return $time;
+
+    }
+
+}
---
+++ tests/Filter/StringToTimeTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Ork
+ *
+ * @package   Ork_Base
+ * @copyright 2015 Alex Howansky (https://github.com/AlexHowansky)
+ * @license   https://github.com/AlexHowansky/ork-base/blob/master/LICENSE MIT License
+ * @link      https://github.com/AlexHowansky/ork-base
+ */
+
+namespace Ork\Tests\Filter;
+
+class StringToTimeTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $filter = null;
+
+    public function setup()
+    {
+        $this->filter = new \Ork\Filter\StringToTime();
+    }
+
+    /**
+     * Test a valid string.
+     */
+    public function testValid()
+    {
+        $this->assertEquals(1278239520, $this->filter->filter('July 4th 2010 6:32am'));
+    }
+
+    /*
+     * Test an existing timestamp.
+     */
+    public function testTimestamp()
+    {
+        $now = time();
+        $this->assertEquals($now, $this->filter->filter($now));
+    }
+
+    /**
+     * Test the special SQL strings.
+     */
+    public function testSql()
+    {
+        $this->assertEquals(time(), $this->filter->filter('now()'), null, 1);
+        $this->assertEquals(time(), $this->filter->filter('CURRENT_TIMESTAMP'), null, 1);
+    }
+
+    /**
+     * Test failure on an invalid string.
+     *
+     * @expectedException \DomainException
+     */
+    public function testFailure()
+    {
+        $this->filter->filter('blarg');
+    }
+
+    /**
+     * Test failure on an invalid timestamp.
+     *
+     * @expectedException \DomainException
+     */
+    public function testNegativeFailure()
+    {
+        $this->filter->filter(-1);
+    }
+
+}
