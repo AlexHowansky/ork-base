2cf79a2d88519845fb85557bf1c2950b7c627d1d
Alex Howansky <alex.howansky@gmail.com>
2015-09-21 22:32:21 +0000

    Added \Image\Inline class.

---
+++ src/Image/Inline.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * Ork
+ *
+ * @package   Ork_Base
+ * @copyright 2015 Alex Howansky (https://github.com/AlexHowansky)
+ * @license   https://github.com/AlexHowansky/ork-base/blob/master/LICENSE MIT License
+ * @link      https://github.com/AlexHowansky/ork-base
+ */
+
+namespace Ork\Image;
+
+/**
+ * Generates inline HTML image markup from image files.
+ */
+class Inline
+{
+
+    /**
+     * Any extra HTML attributes to create the IMG tag with.
+     *
+     * @var string
+     */
+    protected $extraHtml = null;
+
+    /**
+     * The string blob.
+     *
+     * @var string
+     */
+    protected $image = null;
+
+    /**
+     * The MIME type of the image.
+     *
+     * @var string
+     */
+    protected $mimeType = 'image/jpg';
+
+    /**
+     * Allow the object to be referenced as a string.
+     *
+     * @return string The image encoded as an inline IMG tag.
+     */
+    public function __toString()
+    {
+        try {
+            return $this->encode();
+        } catch (\Exception $e) {
+            return '';
+        }
+    }
+
+    /**
+     * Encode the current image as an inline IMG tag.
+     *
+     * @return string The image encoded as an inline IMG tag.
+     */
+    public function encode()
+    {
+        if (empty($this->image) === true) {
+            throw new \RuntimeException('No source image has been specified.');
+        }
+        $str = '<img src="data:' . $this->mimeType . ';base64,' . base64_encode($this->image) . '"';
+        if (empty($this->extraHtml) === false) {
+            $str .= ' ' . trim($this->extraHtml);
+        }
+        $str .= ' />';
+        return $str;
+    }
+
+    /**
+     * Get the extra HTML attributes to be used for this IMG tag.
+     *
+     * @return string The extra HTML attributes to be used for this IMG tag.
+     */
+    public function getExtraHtml()
+    {
+        return $this->extraHtml;
+    }
+
+    /**
+     * Get the image blob.
+     *
+     * @return string The image blob.
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Get the MIME type for this image.
+     *
+     * @return string The MIME type for this image.
+     */
+    public function getMimeType()
+    {
+        return $this->mimeType;
+    }
+
+    /**
+     * Set the extra HTML attributes to be used for this IMG tag.
+     *
+     * @param string $extraHtml The extra HTML attributes.
+     *
+     * @return \Ork\Image\Inline Allow method chaining.
+     */
+    public function setExtraHtml($extraHtml)
+    {
+        $this->extraHtml = $extraHtml;
+        return $this;
+    }
+
+    /**
+     * Set the image source from a string.
+     *
+     * @param string $blob The image.
+     *
+     * @return \Ork\Image\Inline Allow method chaining.
+     */
+    public function setImageFromBlob($blob)
+    {
+        $this->image = $blob;
+        return $this;
+    }
+
+    /**
+     * Set the image source from a file.
+     *
+     * @param string $file The image file.
+     *
+     * @return \Ork\Image\Inline Allow method chaining.
+     */
+    public function setImageFromFile($file)
+    {
+        if (preg_match('/\.([^\.]+)$/', $file, $match) === 1) {
+            $this->setMimeType('image/' . $match[1]);
+        }
+        return $this->setImageFromBlob(file_get_contents($file));
+    }
+
+    /**
+     * Set the MIME type for this image.
+     *
+     * @param string $mimeType The MIME type.
+     *
+     * @return \Ork\Image\Inline Allow method chaining.
+     */
+    public function setMimeType($mimeType)
+    {
+        $this->mimeType = $mimeType;
+        return $this;
+    }
+
+}
---
+++ tests/Image/InlineTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Ork
+ *
+ * @package   Ork_Base
+ * @copyright 2015 Alex Howansky (https://github.com/AlexHowansky)
+ * @license   https://github.com/AlexHowansky/ork-base/blob/master/LICENSE MIT License
+ * @link      https://github.com/AlexHowansky/ork-base
+ */
+
+namespace Ork\Tests\Image;
+
+class InlineTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testSetGetMimeType()
+    {
+        $image = new \Ork\Image\Inline();
+        $this->assertEquals('image/jpg', $image->getMimeType());
+        $image->setMimeType('foo');
+        $this->assertEquals('foo', $image->getMimeType());
+    }
+
+    public function testSetGetExtraHtml()
+    {
+        $image = new \Ork\Image\Inline();
+        $this->assertNull($image->getExtraHtml());
+        $image->setExtraHtml('foo');
+        $this->assertEquals('foo', $image->getExtraHtml());
+    }
+
+    public function testImage()
+    {
+        $hash = '0238e04497807757b13551eb985215a4';
+        $blob = file_get_contents(__DIR__ . '/Fixtures/accept.png');
+        $image = new \Ork\Image\Inline();
+        $image->setImageFromFile(__DIR__ . '/Fixtures/accept.png');
+        $this->assertEquals($blob, $image->getImage());
+        $this->assertEquals($hash, md5((string) $image));
+        $image->setImageFromBlob($blob);
+        $this->assertEquals($blob, $image->getImage());
+        $this->assertEquals($hash, md5((string) $image));
+    }
+
+    public function testExtraHtml()
+    {
+        $image = new \Ork\Image\Inline();
+        $image->setImageFromFile(__DIR__ . '/Fixtures/accept.png');
+        $image->setExtraHtml('class="foo"');
+        $this->assertEquals('1a2e705efa7ed60cccdf83f64f1d2278', md5((string) $image));
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testNoImage()
+    {
+        (new \Ork\Image\Inline())->encode();
+    }
+
+    public function testNoImageString()
+    {
+        $this->assertEquals('', (string) new \Ork\Image\Inline());
+    }
+
+}
