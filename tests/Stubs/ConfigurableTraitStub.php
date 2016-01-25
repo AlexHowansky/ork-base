8bd0b44df8d1a1a8c59abe0a80a6ae5858eb1739
Alex Howansky <alex.howansky@gmail.com>
2016-01-25 19:42:20 +0000

    Copyright update.

--- tests/Stubs/ConfigurableTraitStub.php
+++ tests/Stubs/ConfigurableTraitStub.php
@@ -4,7 +4,7 @@
  * Ork
  *
  * @package   Ork_Base
- * @copyright 2015 Alex Howansky (https://github.com/AlexHowansky)
+ * @copyright 2015-2016 Alex Howansky (https://github.com/AlexHowansky)
  * @license   https://github.com/AlexHowansky/ork-base/blob/master/LICENSE MIT License
  * @link      https://github.com/AlexHowansky/ork-base
  */
